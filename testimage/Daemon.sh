c1ce2f04c95bf4408a682e90e591364a3b62cc10
JayDeeKayG <julus.contact@gmail.com>
2026-02-27 15:00:28 +0000

    added docker compose file and changed scripts to do something

--- testimage/Daemon.sh
+++ testimage/Daemon.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-sleep 1m
-# peak first test script to see even if this minimal thing with almost nothing works
+
+chmod +x /scripts/Prep.sh
+bash /scripts/Prep.sh
+
+while true; do
+  if [ -f "/scripts/ready" ]; then
+    java -jar /minecraft/mc_server.jar
+    break
+  else
+    sleep 0.1
+  fi
+done
+
+# still a basic script
--- testimage/Prep.sh
+++ testimage/Prep.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+VERSION=$MC_VERSION
+LOADER=$MC_LOADER
+
+case $LOADER in
+vanilla)
+  # vanilla do something
+  ;;
+fabric)
+  wget -O mc_server.jar https://meta.fabricmc.net/v2/versions/loader/$MC_VERSION/0.18.2/1.1.1/server/jar
+  ;;
+*)
+  echo "Unknown loader: $LOADER"
+  ;;
+esac
+
+echo "eula=true" >/minecraft/eula.txt
+touch /scripts/ready
