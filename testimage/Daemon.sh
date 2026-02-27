f2337e82882013ec4d69a2391aa23754270c37c3
JayDeeKayG <julus.contact@gmail.com>
2026-02-27 18:48:10 +0000

    merged into one script

--- testimage/Daemon.sh
+++ testimage/Daemon.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 
-chmod +x /scripts/Prep.sh
-bash /scripts/Prep.sh
+VERSION=$MC_VERSION
+LOADER=$MC_LOADER
+LOADER_VERSION=$MC_LOADER_VERSION
 
-while true; do
-  if [ -f "/scripts/ready" ]; then
-    jar_file=$(find /minecraft -maxdepth 1 -type f -name "*.jar" -print -quit)
-    if [ -f "$jar_file" ]; then
-      java -jar "$jar_file" nogui
-    else
-      echo "no jar to peknonc"
-    fi
-    break
-  else
-    sleep 0.1
-  fi
-done
+case $LOADER in
+vanilla)
+  # vanilla do something
+  ;;
+fabric)
+  url="https://meta.fabricmc.net/v2/versions/loader/$VERSION/$LOADER_VERSION/1.1.1/server/jar"
+  wget -O mc_server.jar "$url"
+  ;;
+forge)
+  url="https://maven.minecraftforge.net/net/minecraftforge/forge/$VERSION-$LOADER_VERSION/forge-$VERSION-$LOADER_VERSION-installer.jar"
+  wget -O mc_server.jar "$url"
+  java -jar mc_server.jar --installServer
+  rm /minecraft/mc_server.jar
+  ;;
+*)
+  echo "Unknown loader: $LOADER"
+  ;;
+esac
+
+echo "eula=true" >/minecraft/eula.txt
+
+# running the server jar
+jar_file=$(find /minecraft -maxdepth 1 -type f -name "*.jar" -print -quit)
+if [ -f "$jar_file" ]; then
+  java -jar "$jar_file" nogui
+else
+  echo "No jar file found!"
+fi
 
 # still a basic script
