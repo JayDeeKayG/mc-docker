ba9a4edb566f8b510fafce70e940038829341053
JayDeeKayG <julus.contact@gmail.com>
2026-02-27 15:05:22 +0000

    small changes

--- testimage/Prep.sh
+++ testimage/Prep.sh
@@ -2,13 +2,14 @@
 
 VERSION=$MC_VERSION
 LOADER=$MC_LOADER
+LOADER_VERSION=$MC_LOADER_VERSION
 
 case $LOADER in
 vanilla)
   # vanilla do something
   ;;
 fabric)
-  wget -O mc_server.jar https://meta.fabricmc.net/v2/versions/loader/$MC_VERSION/0.18.2/1.1.1/server/jar
+  wget -O mc_server.jar https://meta.fabricmc.net/v2/versions/loader/$VERSION/$LOADER_VERSION/1.1.1/server/jar
   ;;
 *)
   echo "Unknown loader: $LOADER"
