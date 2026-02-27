51579e677faefd96a14da8926a3ac1fe948be5d5
JayDeeKayG <julus.contact@gmail.com>
2026-02-27 17:26:59 +0000

    forge now works

--- testimage/Daemon.sh
+++ testimage/Daemon.sh
@@ -5,7 +5,12 @@ bash /scripts/Prep.sh
 
 while true; do
   if [ -f "/scripts/ready" ]; then
-    java -jar /minecraft/mc_server.jar
+    jar_file=$(find /minecraft -maxdepth 1 -type f -name "*.jar" -print -quit)
+    if [ -f "$jar_file" ]; then
+      java -jar "$jar_file" nogui
+    else
+      echo "no jar to peknonc"
+    fi
     break
   else
     sleep 0.1
--- testimage/Prep.sh
+++ testimage/Prep.sh
@@ -4,12 +4,21 @@ VERSION=$MC_VERSION
 LOADER=$MC_LOADER
 LOADER_VERSION=$MC_LOADER_VERSION
 
+url=""
+
 case $LOADER in
 vanilla)
   # vanilla do something
   ;;
 fabric)
-  wget -O mc_server.jar https://meta.fabricmc.net/v2/versions/loader/$VERSION/$LOADER_VERSION/1.1.1/server/jar
+  url="https://meta.fabricmc.net/v2/versions/loader/$VERSION/$LOADER_VERSION/1.1.1/server/jar"
+  wget -O mc_server.jar "$url"
+  ;;
+forge)
+  url="https://maven.minecraftforge.net/net/minecraftforge/forge/$VERSION-$LOADER_VERSION/forge-$VERSION-$LOADER_VERSION-installer.jar"
+  wget -O mc_server.jar "$url"
+  java -jar mc_server.jar --installServer
+  rm /minecraft/mc_server.jar
   ;;
 *)
   echo "Unknown loader: $LOADER"
