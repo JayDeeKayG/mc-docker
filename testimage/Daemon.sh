096ac1c7cfd698941c07fcbedb1c9f6391286539
JayDeeKayG <julus.contact@gmail.com>
2026-02-27 19:32:01 +0000

    vanilla now also works

--- testimage/Daemon.sh
+++ testimage/Daemon.sh
@@ -4,9 +4,12 @@ VERSION=$MC_VERSION
 LOADER=$MC_LOADER
 LOADER_VERSION=$MC_LOADER_VERSION
 
+vanilla_url="https://gist.githubusercontent.com/cliffano/77a982a7503669c3e1acb0a0cf6127e9/raw/6c7799c6135292befbc7a07ffde0063ecaa506c4/minecraft-server-jar-downloads.md"
+
 case $LOADER in
 vanilla)
-  # vanilla do something
+  url=$(wget -qO- "$vanilla_url" | grep -i "$VERSION" | awk -F'|' '{print $3}' | xargs)
+  wget -O mc_server.jar "$url"
   ;;
 fabric)
   url="https://meta.fabricmc.net/v2/versions/loader/$VERSION/$LOADER_VERSION/1.1.1/server/jar"
